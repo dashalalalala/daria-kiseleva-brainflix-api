4385c5486731f6bc757d930c7264139d1ba97cd2
dashalalalala <dchernikaa@gmail.com>
2023-03-24 20:17:17 -0700

    added image placeholder, added POST endpoint for comments

--- index.js
+++ index.js
@@ -4,19 +4,31 @@ const cors = require("cors");
 const fs = require("fs");
 const PORT = 8080;
 const URL = "http://localhost:";
-const videoRoutes = require("./routes/videos")
+const videoRoutes = require("./routes/videos");
 
 app.use(cors());
 app.use(express.json());
 
-// This middleware allows us to serve static files from a folder.
+// Static Folder
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 	res.send("Hello World");
 });
 
-app.use("/videos", videoRoutes)
+// This middleware checks if we're getting JSON headers on our POST requests
+app.use((req, res, next) => {
+	if (
+		req.method === "POST" &&
+		req.headers["content-type"] !== "application/json"
+	) {
+		return res.status(400).send("Hey, you need to give me proper JSON");
+	}
+
+	next();
+});
+
+app.use("/videos", videoRoutes);
 
 app.listen(PORT, () => {
 	console.log(`App is running on ${PORT}`);
--- routes/videos.js
+++ routes/videos.js
@@ -2,18 +2,16 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const uuid4 = require("uuid4");
-// const { v4: uuidv4 } = require("uuid");
 
 function readVideos() {
 	const videosJSON = fs.readFileSync("./data/video-details.json");
 	const parsedVideos = JSON.parse(videosJSON);
 	return parsedVideos;
 }
-
 readVideos();
 
 router.use((req, res, next) => {
-	console.log("Middleware from notes router");
+	console.log("Middleware from videos router");
 	next();
 });
 
@@ -35,20 +33,40 @@ router.post("/", (req, res) => {
 		id: uuid4(),
 		title: req.body.title,
 		channel: "New Channel",
-		image: "../public/images/image0.jpeg",
+		image: "http://localhost:8080/filler-image/filler.png",
 		description: req.body.description,
 		views: "0",
 		likes: "0",
 		duration: "0",
 		video: "https://project-2-api.herokuapp.com/stream",
-		timestamp: "",
+		timestamp: req.body.timestamp,
+		comments: [],
 	};
-
-	console.log(newVideo);
+	console.log("new video", newVideo);
 	const videos = readVideos();
 	videos.push(newVideo);
 	fs.writeFileSync("./data/video-details.json", JSON.stringify(videos));
 	res.status(200).json(newVideo);
 });
+
+//POST Endpoint for comments
+router.post("/:videoId/comments", (req, res) => {
+	const newComment = {
+		comment: req.body.comment,
+		id: uuid4(),
+		likes: req.body.likes,
+		name: "New User",
+		timestamp: Date.now(),
+	};
+	console.log("new comment", newComment);
+	const videos = readVideos();
+	const singleVideo = videos.find((video) => video.id === req.params.videoId);
+	singleVideo.comments.push(newComment);
+	fs.writeFileSync("./data/video-details.json", JSON.stringify(videos));
+	res.status(200).json(newComment);
+});
+
+//DELETE Endpoint for comments
+router.delete("/:videoId/comments/:commentId"), (req, res) => {};
 
 module.exports = router;
